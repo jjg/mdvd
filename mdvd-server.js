d2ef1b1b834ce8e54f1eea77b3c322a0fae22337
Jason J. Gullickson <jason.gullickson@gmail.com>
2014-07-09 12:44:02 -0500

    added basic mplayer initialization

--- mdvd-server.js
+++ mdvd-server.js
@@ -1,14 +1,37 @@
 var restify = require('restify');
+var spawn = require('child_process').spawn;
 
+// init server
 var server = restify.createServer();
 
+// todo: init mplayer
+var mplayer = spawn('/usr/bin/mplayer',['dvd:////dev/dvd','-slave']);
+
+// handlers
+function processCommand(req, res, next){
+	
+	// debug
+	console.log('received command: ' + req.params.commandName);
+
+	mplayer.stdin.write(req.params.commandName);
+
+	res.send(200);
+	return next();
+}
+
+mplayer.stdout.on('data', function(data){
+	console.log(data.toString());
+});
+
 // endpoints
+server.get({path:'/command/:commandName', version:'1.0.0'}, processCommand);
 
 // static content
 server.get(/\/player\/?.*/, restify.serveStatic({
 	directory:'./static'
 }));
 
+// start the api server
 var port = process.env.port || 5000;
 server.listen(port, function(){
 	console.log('%s listening at ', server.name, server.url);
