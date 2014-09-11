211762d6f82eea802da836061120ec9f406522a6
Jason Gullickson <jason.gullickson@gmail.com>
2014-09-11 14:10:48 -0500

    control ffmpeg launch via ffserver config, add play/stop controls and integrate some playback initialization into html client-side

--- mdvd-server.js
+++ mdvd-server.js
@@ -5,7 +5,7 @@ var spawn = require('child_process').spawn;
 var server = restify.createServer();
 
 // init mplayer
-var player = spawn('./mplay_title', ['/dev/dvd', '1', '1', '1', 'mplay_title.out']);
+var player = null;
 
 // handlers
 function processCommand(req, res, next){
@@ -13,15 +13,37 @@ function processCommand(req, res, next){
 	// debug
 	console.log('received command: ' + req.params.commandName);
 
-	player.stdin.write(req.params.commandName + '\n');
+	switch(req.params.commandName){
+		case 'play':
+			// play the disc
+ 			if(!player){
+        		player = spawn('./mplay_title', ['/dev/dvd', '1', '2', '1', 'mplay_title.out']);
+    		}
+			break;
+
+		case 'stop':
+			// stop playback if playing
+			if(player){
+				player.kill();
+				player = null;
+			}
+			break;
+
+		default:
+			// who knows?
+			console.log('unknown command received');
+	}
+
+	//player.stdin.write(req.params.commandName + '\n');
 
 	res.send(200);
 	return next();
 }
-
+/*
 player.stdout.on('data', function(data){
 	console.log(data.toString());
 });
+*/
 
 // endpoints
 server.get({path:'/command/:commandName', version:'1.0.0'}, processCommand);
